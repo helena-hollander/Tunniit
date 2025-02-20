8d0929df4a86b5b5bb644c0e8a50ba2e0e3f771a
Helena Hollænder <helena.hollaender@gmail.com>
2025-02-20 10:35:30 +0100

    torsdag_jaja

--- src/components/molecules/imageContainers/ImageParallaxJs.vue
+++ src/components/molecules/imageContainers/ImageParallaxJs.vue
@@ -1,30 +1,41 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Image from '@/components/atoms/Image.vue';
 
 
-window.addEventListener('scroll', function(e) {
-    const target = this.document.querySelectorAll('.para');
+const setInitialPosition = () => {
+    const target = document.querySelectorAll('.para');
+    target.forEach(element => {
+        const initialY = element.dataset.initialY || '0px';
+        element.style.transform = `translate3d(0px, ${initialY}, 0px)`;
+    });
+};
 
-    var index = 0,length = target.length;
-    for(index; index < length; index++){
-        var pos = this.window.pageYOffset * target[index].dataset.rate;
-        target[index].style.transform = 'translate3d(0px, '+pos+'px, 0px)';
-    }
+const updatePositionOnScroll = () => {
+    const target = document.querySelectorAll('.para');
+    target.forEach(element => {
+        const rate = parseFloat(element.dataset.rate) || 0;
+        const initialY = parseFloat(element.dataset.initialY) || 0;
+        const pos = window.pageYOffset * rate;
+        element.style.transform = `translate3d(0px, ${initialY + pos}px, 0px)`;
+    });
+};
 
-    // var scrolled = window.pageYOffset;
-    // var rate = scrolled * 0.2;
-    // target.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
-    // console.log('scrolling');
+onMounted(() => {
+    setInitialPosition();
+    window.addEventListener('scroll', updatePositionOnScroll);
 });
 
 </script>
 <template>
     <header class="h-[200vh]">
-        <!-- <Image imagePath="src/assets/images/mirror.jpg" class="para"></Image> -->
-        <Image imagePath="src/assets/images/smoke1.png" class="para absolute" data-rate="0.5"></Image> 
-        <Image imagePath="src/assets/images/smoke2.png" class="para absolute" ></Image>
-        <Image imagePath="src/assets/images/smoke3.png" class="para absolute" data-rate="-1"></Image>
+        <Image imagePath="src/assets/images/smoke1.png" class="para absolute" ></Image>
+        <Image imagePath="src/assets/images/smoke2.png" class="para absolute" data-initial-y="-10px" data-rate="0.1"></Image>
+        <Image imagePath="src/assets/images/smoke3.png" class="para absolute" ></Image>
+   
         <!-- High date-rate number,the quicker it scrolls upwards. 1 keeps it stuck. 0 or no prop keeps it at regular pace-->
     </header>
+
+    <!-- Hvordan giver jeg image ny position, uden min scroll index overule, men 'tilføjer' -->
+    <!-- Composable function? Tilgå funktionen, selvom den ref til classer i hvert billede?-->
 </template>
