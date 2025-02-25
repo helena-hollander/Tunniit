8e0d87247fb7b0308ad05ee938de51273557473b
Helena Hollænder <helena.hollaender@gmail.com>
2025-02-25 11:45:57 +0100

    frokost_tirsdag

--- src/components/molecules/ImageShards.vue
+++ src/components/molecules/ImageShards.vue
@@ -9,6 +9,7 @@ import Image from '@/components/atoms/Image.vue';
      <Image src="src/assets/images/mirrorShards/duo1.png" :initialY="'-4'" :rate="0.005" :initialX="'-4'" :rateX="0.008"></Image>
      <Image src="src/assets/images/mirrorShards/decision1.png" :initialY="'1'" :rate="0" :initialX="'0'" :rateX="0"></Image>
      <Image src="src/assets/images/mirrorShards/coat1.png" :initialY="'100'" :rate="-0.01" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/mirrorShards/couch1.png" :initialY="'136'" :rate="0" :initialX="'0'" :rateX="0"></Image>
 
     <!-- High date-rate number,the quicker it scrolls upwards. 1 keeps it stuck. 0 or no prop keeps it at regular pace-->
 </header>
