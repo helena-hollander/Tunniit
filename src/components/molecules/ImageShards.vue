74a0735aefd9e9e93e58b17d4432b09aced8bac2
Helena Hollænder <helena.hollaender@gmail.com>
2025-02-28 14:17:53 +0100

    dialog_del_et_macbook_format

--- src/components/molecules/ImageShards.vue
+++ src/components/molecules/ImageShards.vue
@@ -9,12 +9,12 @@ import Image from '@/components/atoms/Image.vue';
      <Image src="src/assets/images/sketchShards/decision_sketch01.png" :initialY="'28'" :rate="0" :initialX="'0'" :rateX="0"></Image>
      <Image src="src/assets/images/sketchShards/duo_sketch01.png" :initialY="'16'" :rate="0.012" :initialX="'-4'" :rateX="0.008"></Image>
      <Image src="src/assets/images/sketchShards/coat_sketch01.png" :initialY="'116'" :rate="-0.01" :initialX="'0'" :rateX="0"></Image>
-     <Image src="src/assets/images/sketchShards/couch_sketch01.png" :initialY="'162'" :rate="0" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/sketchShards/couch_sketch01.png" :initialY="'166'" :rate="0" :initialX="'4'" :rateX="-0.002"></Image>
      <Image src="src/assets/images/sketchShards/eyes_sketch01.png" :initialY="'170'" :rate="0.015" :initialX="'6'" :rateX="-0.0015"></Image>
      <Image src="src/assets/images/sketchShards/needle_sketch01.png" :initialY="'296'" :rate="-0.025" :initialX="'8'" :rateX="-0.002"></Image>
-     <Image src="src/assets/images/sketchShards/eye_sketch0102.png" :initialY="'228'" :rate="0.0151" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/sketchShards/eye_sketch0102.png" :initialY="'228'" :rate="0.0158" :initialX="'0'" :rateX="0"></Image>
      <!-- <Image src="src/assets/images/sketchShards/shards_sketch01.png" :initialY="'1'" :rate="0.01" :initialX="'0'" :rateX="0"></Image> -->
-     <Image src="src/assets/images/sketchShards/shards_sketch01.png" :initialY="'202'" :rate="0" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/sketchShards/shards_sketch01.png" :initialY="'202'" :rate="-0.004" :initialX="'0'" :rateX="0"></Image>
 
     <!-- High date-rate number,the quicker it scrolls upwards. 1 keeps it stuck. 0 or no prop keeps it at regular pace-->
 </header>
--- src/components/molecules/TextLayout.vue
+++ src/components/molecules/TextLayout.vue
@@ -14,6 +14,18 @@ import { ref, onMounted, onUnmounted } from 'vue'
     <p class="w-[100vw] h-[240px] col-span-3 col-start-2 content-end translate-x-[-40px]">ER DU KLAR?</p>
     <p class="w-[100vw] h-[640px] col-span-2 col-start-3 content-end justify-self-end text-right text-sky-900 text-dialog2/[0.8] ">OKAY.</p>
     <!-- dialog 3 -->
-    
+    <p class="w-[100vw] h-[1000px] col-span-3 col-start-2 content-end translate-x-[-40px]">DU KAN BARE SMIDE JAKKEN I HJØRNET.<br>ER DU NERVØS?</p>
+    <p class="w-[100vw] col-span-2 col-start-4 content-end justify-self-end text-right text-dialog2/[0.8] ">JA, LIDT MÅSKE.</p>
+    <p class="w-[100vw] col-span-3 col-start-2 content-end translate-x-[-40px]">DET BLIR' SÅ FEDT.<br>JEG FINDER LIGE GREJET FREM. 2 SEK.</p>
+    <p class="w-[100vw] h-[560px] col-span-3 col-start-2 content-end translate-x-[-40px]">LIGGER DU OKAY?</p>
+    <p class="w-[100vw] h-[60px] col-span-2 col-start-4 content-end justify-self-end text-right text-dialog2/[0.8] ">MH-HMM.</p>
+    <p class="w-[100vw] col-span-3 col-start-2 content-end translate-x-[-40px]">HERE WE GO.<br>PRØV OM DU KAN HUSKE AT TRÆKKE VEJRET?</p>
+    <p class="w-[100vw] col-span-2 col-start-4 content-end justify-self-end text-right text-dialog2/[0.8] ">JAER.</p>
+    <p class="w-[100vw] col-span-3 col-start-2 content-end translate-x-[-40px]"></p>
+    <p class="w-[100vw] h-[280px] col-span-2 col-start-5 content-end justify-self-end text-right text-dialog2/[0.8] translate-x-[60px]">ARGH!</p>
+    <p class="w-[100vw] col-span-2 col-start-5 content-end justify-self-end text-right text-dialog2/[0.8] translate-x-[60px]"></p>
+    <p class="w-[100vw] h-[180px] col-span-2 col-start-7 content-end justify-self-end text-right text-dialog2/[0.8] ">PFUHHHHH...</p>
+    <p class="w-[100vw] h-[640px] col-span-3 col-start-1 content-end translate-x-[-40px] -rotate-[0.28rad]">FLOT QIVI. DYB INDÅNDING, TÆNK PÅ NOGET ANDET...</p>
+    <p class="w-[100vw] h-[600px] col-span-3 col-start-2 content-end translate-x-[-40px]"></p>
 </div>
 </template>
