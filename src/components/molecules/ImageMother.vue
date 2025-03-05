c12623985e4d4deb8bc595aa00baebfd634f3246
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-05 10:07:25 +0100

    onsdag_middag

--- src/components/molecules/ImageMother.vue
+++ src/components/molecules/ImageMother.vue
@@ -4,9 +4,14 @@ import Image from '@/components/atoms/Image.vue';
 </script>
 
 <template>
-  <div class="h-[500vh] relative"> 
-    <Image src="src/assets/images/del3Sketch/mother0201.png" :initialY="'-84'" :rate="0.005" :initialX="'0'" :rateX="0"></Image>
-     <Image src="src/assets/images/del3Sketch/scream0201.png" :initialY="'-38'" :rate="0.0018" :initialX="'-0.5'" :rateX="0"></Image>
+  <div class="h-[800vh] relative"> 
+    <Image src="src/assets/images/del3Sketch/mother0201.png" :initialY="'-84'" :rate="0.005" :initialX="'0'" :rateX="0" class="z-10"></Image>
+     <Image src="src/assets/images/del3Sketch/scream0201.png" :initialY="'-38'" :rate="0.0018" :initialX="'-0.5'" :rateX="0" class="z-10"></Image>
+     <Image src="src/assets/images/del3Sketch/motherHair0202.png" :initialY="'197'" :rate="0.0032" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/del3Sketch/motherHair0201.png" :initialY="'326'" :rate="-0.0045" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/del3Sketch/mother0202.png" :initialY="'150'" :rate="0.0032" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/del3Sketch/silhuet0201.png" :initialY="'352'" :rate="-0.0045" :initialX="'0'" :rateX="0"></Image>
+     <Image src="src/assets/images/del3Sketch/cutHand0201.png" :initialY="'626'" :rate="-0.018" :initialX="'0'" :rateX="0"></Image>
 
    
   </div>
