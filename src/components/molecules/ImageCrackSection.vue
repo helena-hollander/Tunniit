b5293f21fad56d1f61dd4f3b949f5870053daec8
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-05 16:12:23 +0100

    onsdag_pause

--- src/components/molecules/ImageCrackSection.vue
+++ src/components/molecules/ImageCrackSection.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import ImageCrack from '../atoms/ImageCrack.vue';
+import useCrack from '@/composables/useCrack.js';
+const { divHasBg } = useCrack();
 
 </script>
 
 <template>
-    <div class="h-[200vh] w-full bg-sky-900">
-
+    <div class="h-[300vh] w-full ">
         <div >
-
-            <ImageCrack class="w-full object-cover" src="/images/del4Sketch/cry0301.png" original-src="/images/del4Sketch/cry0301.png" new-src="/images/del4Sketch/wakingShard0301.png" />
+            <ImageCrack class="w-full object-cover z-20" src="/images/del4Sketch/cry0301.png" original-src="/images/del4Sketch/cry0301.png" new-src="/images/del4Sketch/wakingShard0301.png" />
         </div>
+        <div class="h-[180vh] w-full bg-sky-900 top-0"></div>
 
     </div>
 </template>
--- src/components/molecules/ImageMother.vue
+++ src/components/molecules/ImageMother.vue
@@ -4,7 +4,7 @@ import Image from '@/components/atoms/Image.vue';
 </script>
 
 <template>
-  <div class="h-[840vh] relative"> 
+  <div class="h-[810vh] relative"> 
     <Image src="/images/del3Sketch/bg03.png" :initialY="'-380'" :rate="0.03" :initialX="'0'" :rateX="0"></Image>
     <Image src="/images/del3Sketch/mother0201.png" :initialY="'-84'" :rate="0.005" :initialX="'0'" :rateX="0" class="z-10"></Image>
      <Image src="/images/del3Sketch/scream0201.png" :initialY="'-38'" :rate="0.0018" :initialX="'-0.5'" :rateX="0" class="z-10"></Image>
