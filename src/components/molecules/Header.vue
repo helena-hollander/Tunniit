785cdd48f7e32741c0f3ca1e805478587cf9be7d
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-08 20:03:00 +0100

    header_fix_to

--- src/components/molecules/Header.vue
+++ src/components/molecules/Header.vue
@@ -4,11 +4,11 @@
 <template>
 <div class="gap-2 w-full h-[160px] grid grid-cols-11 grid-rows-5 fixed z-20 justify-items-center text-white font-dialog text-2xl ">
   
-  <!-- <img src="/images/logoShadow.png" class="w-[8vw] pl-2 m-2  col-span-1 hover:opacity-60"></img> -->
   <nav class="m-4 hover:underline justify-self-start self-top col-start-1 row-start-1 col-span-2 text-base/[1] pl-2 pt-4 hover:text-sky-700">Inuusuttunik Noorliit<br><span class="italic text-base">UNGE DRØMME</span></nav>
   <nav class="m-4 hover:underline justify-self-center col-span-3 col-start-5 row-start-1 text-base hover:text-sky-700"> < Kapitel 4 > </nav>
   <nav class=" hover:underline justify-self-center self-start m-2 row-start-2 col-start-6 text-4xl italic hover:text-sky-700">'TUNNIIT'</nav>
-
+  
+  <img src="/images/logoShadow.png" class="w-[3vw] pt-6  col-span-1 col-start-11 hover:opacity-60"></img>
 
 </div>
 </template>
